4869bc15848da06798723160ebd80d129ff3cd29
“abeerscode” <md.abeerhasan71@gmail.com>
2025-10-12 05:33:16 +0600

    Leap Year Checker

--- ptc2/Program.cs
+++ ptc2/Program.cs
@@ -5,15 +5,16 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Eneter a year:");
+        Console.WriteLine("Enter a year:");
         int year = int.Parse(Console.ReadLine());
+
         if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
         {
-            Console.WriteLine($"{year} is a leap year.");
+            Console.WriteLine(year + " is a leap year.");
         }
         else
         {
-            Console.WriteLine($"{year} is not a leap year.");
+            Console.WriteLine(year + " is not a leap year.");
         }
     }
 }
