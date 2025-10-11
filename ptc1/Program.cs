3306c09db975a178781c64acb3c50b3d69fd3c26
“abeerscode” <md.abeerhasan71@gmail.com>
2025-10-12 04:57:12 +0600

    fixed

---
+++ ptc1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Print "hello world"
+
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ ptc2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Leap Year
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Eneter a year:");
+        int year = int.Parse(Console.ReadLine());
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        {
+            Console.WriteLine($"{year} is a leap year.");
+        }
+        else
+        {
+            Console.WriteLine($"{year} is not a leap year.");
+        }
+    }
+}
