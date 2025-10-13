b2ab64b3a3ccfdada00c1c4b2134ef69fc8b9870
“abeerscode” <md.abeerhasan71@gmail.com>
2025-10-13 13:16:28 +0600

    Adding Factorial program

---
+++ ptc5/Program.cs
@@ -0,0 +1,18 @@
+﻿// Factorial of a number
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number :");
+        int n = int.Parse(Console.ReadLine());
+
+        long fact = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            fact = fact * i;
+        }
+        Console.WriteLine("Factorial of " + n + " is : " + fact);
+    }
+}        
