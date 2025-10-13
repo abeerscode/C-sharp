252b5505c734031a58c50730ca558c7c19dd9ff7
“abeerscode” <md.abeerhasan71@gmail.com>
2025-10-13 12:54:07 +0600

    Fibonacci

---
+++ ptc4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Fibonacci Number 
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter how many fibonacci number to display :");
+        int n = int.Parse(Console.ReadLine());
+        int a = 0, b = 1, c;
+        Console.WriteLine("Fibonacci Series:");
+        Console.Write(a + " " + b + " ");          //printing 0 and 1
+        for (int i = 2; i < n; i++)                //loop starts from 2 because 0 and 1 are already printed    
+        {
+            c = a + b;
+            Console.Write(c + " ");
+            a = b;
+            b = c;
+        }
+    }    
+}
