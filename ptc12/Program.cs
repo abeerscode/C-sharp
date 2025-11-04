95c31c4620e2cc293170dc99df4d522c343f6e4f
“abeerscode” <md.abeerhasan71@gmail.com>
2025-11-04 19:26:28 +0600

    .

--- ptc12/Program.cs
+++ ptc12/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Rectangle
+{
+    private int length;
+    private int width;
+    private int area;
+    public int Length
+    {
+        get
+        {
+            return length;
+        }
+        set
+        {
+            if (value <= 0)
+            {
+                Console.Write("Invalid input");
+            }
+            else
+            {
+                length = value;
+            }
+        }
+    }
+    public int Width
+    {
+        get
+        {
+            return width;
+        }
+        set
+        {
+            if (value <= 0)
+            {
+                Console.Write("Invalid input");
+            }
+            else
+            {
+                width = value;
+            }
+        }
+    }
+    public int Area
+    {
+        get
+        {
+            return length * width;
+        }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Rectangle r1 = new Rectangle();
+        r1.Length = 3;
+        r1.Width = 4;
+        Console.Write("Area is : " + r1.Area);
+    }
+}
